bc8a74bc297e6bc994c6a8cd6fa60da5c5ce8c87
Maxim Predybailo <predmaxim@ya.ru>
2025-05-26 18:01:38 +0500

    refactor: simplify App component by removing unnecessary div wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,7 @@ import TimeSegmentsBlock from "@components/TimeSegmentsBlock";
 import { demo } from "@/mocks";
 
 const App: React.FC = () => {
-  return (
-    <div>
-      <TimeSegmentsBlock segments={demo} />
-    </div>
-  );
+  return <TimeSegmentsBlock segments={demo} />;
 };
 
 export default App;
