a2237484aac19afe5b734d39c1e72edd9f7fb5ad
Maxim Predybailo <predmaxim@ya.ru>
2025-05-26 22:32:27 +0500

    feat: add global styles with SCSS and normalize.css to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import TimeSegmentsBlock from "@components/TimeSegmentsBlock";
 import { demo } from "@/mocks";
+import "@styles/index.scss";
 
 const App: React.FC = () => {
   return <TimeSegmentsBlock segments={demo} />;
